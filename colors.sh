ee44af91c179e6a0018f3cf2e8a385f249b299b4
VladimirPapazov88 <papazovvladimir88@gmail.com>
2023-07-23 19:01:15 +0300

    add colorscheme

--- colors.sh
+++ colors.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-INNER='#434c5e'
-CLEAR='#2e3440'
-DEFAULT='#2e3440'
-TEXT='#eceff4'
-WRONG='#bf616a'
-VERIFYING='#a3be8c'
-
+source "colorschemes/Tokyo Night Storm.sh"
---
+++ colorschemes/nord.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+INNER='#434c5e'
+CLEAR='#2e3440'
+DEFAULT='#2e3440'
+TEXT='#eceff4'
+WRONG='#bf616a'
+VERIFYING='#a3be8c'
+
